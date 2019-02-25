0398738a7653902de5ca08847ac1fd1dfbc2f2bb
Aleksey <mykms@ya.ru>
2019-02-25 16:58:01 +0300

    Update SimpleFragmentDialog.kt

--- app/src/main/java/com/task2trip/android/UI/Dialog/SimpleFragmentDialog.kt
+++ app/src/main/java/com/task2trip/android/UI/Dialog/SimpleFragmentDialog.kt
@@ -30,3 +30,71 @@ class SimpleFragmentDialog: DialogFragment() {
         //
     }
 }
+/*
+public class SimpleDialogFragment extends DialogFragment {
+
+    @Nullable
+    private String title;
+    @Nullable
+    private String description;
+    @Nullable
+    private String positiveButtonName;
+    @Nullable
+    private String negativeButtonName;
+
+    public static SimpleDialogFragment newInstance(@NonNull String title,
+                                                   @NonNull String description,
+                                                   @NonNull String positiveButtonName,
+                                                   @NonNull String negativeButtonName) {
+        Bundle args = new Bundle();
+        args.putString(AppExtras.EXTRA_DIALOG_TITLE, title);
+        args.putString(AppExtras.EXTRA_DIALOG_DESCRIPTION, description);
+        args.putString(AppExtras.EXTRA_DIALOG_POSITIVE_BUTTON_TITLE, positiveButtonName);
+        args.putString(AppExtras.EXTRA_DIALOG_NEGATIVE_BUTTON_TITLE, negativeButtonName);
+        SimpleDialogFragment fragment = new SimpleDialogFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        readArguments(getArguments());
+    }
+
+    private void readArguments(@Nullable Bundle arguments) {
+        if (arguments != null) {
+            title = arguments.getString(AppExtras.EXTRA_DIALOG_TITLE, "");
+            description = arguments.getString(AppExtras.EXTRA_DIALOG_DESCRIPTION, "");
+            positiveButtonName = arguments.getString(AppExtras.EXTRA_DIALOG_POSITIVE_BUTTON_TITLE, "");
+            negativeButtonName = arguments.getString(AppExtras.EXTRA_DIALOG_NEGATIVE_BUTTON_TITLE, "");
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(title)
+                .setMessage(description)
+                .setCancelable(false)
+                .setPositiveButton(positiveButtonName, (dialog, id) -> {
+                    dialog.cancel();
+                    onResult(Activity.RESULT_OK);
+                })
+                .setNegativeButton(negativeButtonName, (dialog, id) -> {
+                    dialog.cancel();
+                    onResult(Activity.RESULT_CANCELED);
+                });
+        return builder.create();
+    }
+
+    private void onResult(int resultCode) {
+        Fragment targetFragment = getTargetFragment();
+        if (targetFragment != null) {
+            targetFragment.onActivityResult(getTargetRequestCode(), resultCode, null);
+        }
+    }
+
+}
+*/
