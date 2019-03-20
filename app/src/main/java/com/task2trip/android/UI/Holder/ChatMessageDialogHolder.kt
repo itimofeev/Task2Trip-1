9f3ad99b12a790c28ce56730cd7cc21fa7bbda56
Aleksey <mykms@ya.ru>
2019-03-20 15:55:43 +0300

    Create ChatMessageDialogHolder.kt

---
+++ app/src/main/java/com/task2trip/android/UI/Holder/ChatMessageDialogHolder.kt
@@ -0,0 +1,29 @@
+package com.task2trip.android.UI.Holder
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.task2trip.android.Model.ImageLoader.ImageLoader
+import com.task2trip.android.R
+
+class ChatMessageDialogHolder(itemView: View) : BaseHolder<TaskCategory>(itemView) {
+    private var tvCategoryName: TextView? = null
+    private var ivCategoryImage: ImageView? = null
+
+    init {
+        tvCategoryName = itemView.findViewById(R.id.tvCategoryName)
+        ivCategoryImage = itemView.findViewById(R.id.ivCategoryImage)
+    }
+
+    override fun setData(item: TaskCategory) {
+        tvCategoryName?.text = item.defaultValue
+        ivCategoryImage?.let {
+            ImageLoader(item.imageUrl, it)
+        }
+    }
+
+    override fun setItemClickListener(listener: View.OnClickListener?) {
+        itemView.setOnClickListener(listener)
+        tvCategoryName?.setOnClickListener(listener)
+    }
+}
