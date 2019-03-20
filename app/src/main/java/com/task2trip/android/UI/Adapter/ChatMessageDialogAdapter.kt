1990925fa18d83240cc193099776082e1b09c99c
Aleksey <mykms@ya.ru>
2019-03-20 15:59:25 +0300

    Update ChatMessageDialogAdapter.kt

--- app/src/main/java/com/task2trip/android/UI/Adapter/ChatMessageDialogAdapter.kt
+++ app/src/main/java/com/task2trip/android/UI/Adapter/ChatMessageDialogAdapter.kt
@@ -3,9 +3,9 @@ ackage com.task2trip.android.UI.Adapter
 import android.view.View
 import com.task2trip.android.R
 
-class ChatMessageDialogAdapter(items: List<TaskCategory>):
-    BaseListAdapter<ChatMessageDialogHolder, TaskCategory>(items) {
-    private var listener: ItemClickListener<TaskCategory>? = null
+class ChatMessageDialogAdapter(items: List<ChatMessage>):
+    BaseListAdapter<ChatMessageDialogHolder, ChatMessage>(items) {
+    private var listener: ItemClickListener<ChatMessage>? = null
 
     override fun setLayoutRes(): Int {
         return R.layout.item_task_add_category
@@ -15,11 +15,11 @@ class ChatMessageDialogAdapter(items: List<TaskCategory>):
         return ChatMessageDialogHolder(view)
     }
 
-    override fun onItemClicked(item: TaskCategory, position: Int) {
+    override fun onItemClicked(item: ChatMessage, position: Int) {
         this.listener?.onItemClick(item, position)
     }
 
-    override fun setClickListener(listener: ItemClickListener<TaskCategory>?) {
+    override fun setClickListener(listener: ItemClickListener<ChatMessage>?) {
         this.listener = listener
     }
 }
