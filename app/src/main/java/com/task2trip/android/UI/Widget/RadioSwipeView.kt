6865796cda0ce1e8e51ae5db55bd4c0244abc480
ilya.timofeev <ilya.ufo@gmail.com>
2019-04-11 00:45:14 +0300

    fixes for 01 onboarding

--- app/src/main/java/com/task2trip/android/UI/Widget/RadioSwipeView.kt
+++ app/src/main/java/com/task2trip/android/UI/Widget/RadioSwipeView.kt
@@ -16,14 +16,14 @@ class RadioSwipeView(@NonNull context: Context, attrs: AttributeSet? = null) : R
         val radioButton = ArrayList<RadioButton>()
         for (i in 0 until mCount) {
             try {
-                val hw = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8f, resources.displayMetrics).toInt()
+                val hw = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10f, resources.displayMetrics).toInt()
                 val rb = RadioButton(context)
                 rb.id = i
                 rb.buttonDrawable = null
                 radioButton.add(rb)
                 val params = RadioGroup.LayoutParams(hw, hw)
-                params.leftMargin = hw / 2
-                params.rightMargin = hw / 2
+                params.leftMargin = hw / 4
+                params.rightMargin = hw / 4
                 radioButton[i].layoutParams = params
                 if (i == 0) {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
